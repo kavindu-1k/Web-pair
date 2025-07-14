c8da92601da71332acd1c6e75e1a6c9a244fd23f
kavindu-1k <kavindukalpana200922@gmail.com>
2025-07-14 12:32:09 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: '//=====//', // Replace with your Mega email
-    password: '//=====//', // Replace with your Mega password
+    email: 'kavindukalpana0622@gmail.com', // Replace with your Mega email
+    password: 'kaviya20090622', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
